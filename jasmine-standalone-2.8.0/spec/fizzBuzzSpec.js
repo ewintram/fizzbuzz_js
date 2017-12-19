048deb88a30144905b83fc3d2c244e75e6b9c532
kkumshayev <kumshkesh@gmail.com>
2017-12-19 15:37:40 +0000

    failing unit test for a number not divisible by either 3 or 5

--- jasmine-standalone-2.8.0/spec/fizzBuzzSpec.js
+++ jasmine-standalone-2.8.0/spec/fizzBuzzSpec.js
@@ -22,6 +22,9 @@ describe('javabuzz', function() {
       it('"JavaBuzz" when a number is divisible by 15', function() {
         expect(javabuzz.says(15)).toEqual("JavaBuzz");
       });
+      it('number when not divisible by 5 or 3', function() {
+        expect(javabuzz.says(4)).toEqual(4);
+      });
     });
 
 });
